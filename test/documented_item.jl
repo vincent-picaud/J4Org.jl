f615683a740db306299c4aa20b0633b4bd56e7de
Picaud Vincent <picaud.vincent@gmail.com>
2018-05-05 09:54:44 +0200

    complete macro support

--- test/documented_item.jl
+++ test/documented_item.jl
@@ -63,3 +63,13 @@ end;
     @test org_string_comment(di,[di],[di],"","BoxingModule") == "#+BEGIN_QUOTE\nThis is an variable example [[target][variable A]]\n#+END_QUOTE\n"
     @test org_string_code(di) == "#+BEGIN_SRC julia :eval never :exports code\nA\n#+END_SRC\n"
  end;
+
+
+@testset "macro" begin
+    filename="$(dirname(@__FILE__))/code_examples/macro.jl"
+    t=tokenized(readstring(filename))
+    r=find_tag(t,1)
+    di=create_documented_item(t,tag_idx(r),filename=filename)
+    @test org_string_comment(di,[di],[di],"","BoxingModule") == "#+BEGIN_QUOTE\nThis is an macro example [[target][@swap]]\n#+END_QUOTE\n"
+    @test org_string_code(di) == "#+BEGIN_SRC julia :eval never :exports code\nmacro swap(x,y)\n#+END_SRC\n"
+ end;
